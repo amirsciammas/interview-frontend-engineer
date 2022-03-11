f2d4acb83e9398fc2e011d0ebaba69e39586db94
Mark Rivera <mark.rivera@wizeline.com>
2022-03-11 16:15:14 -0600

    [dev] Display users in a dropdown

--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -1,26 +1,49 @@
 import { FunctionComponent, useEffect, useState } from "react";
-import { getPosts, getUsers } from "../../utils/api";
-import { Post, User } from "../../utils/api.types";
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { getUsers } from "../../utils/api";
+import { User } from "../../utils/api.types";
 
 const Container: FunctionComponent = () => {
   const [users, setusers] = useState([] as User[])
-  const [posts, setposts] = useState([] as Post[])
+  const [selectedUser, setselectedUser] = useState('')
 
   useEffect(() => {
     getUsers().then(users => setusers(users as User[]));
-    getPosts().then(posts => setposts(posts as Post[]));
   }, []);
-  
-  const renderTitles = (titles: User[] | Post[]) => {
-    return titles.map(title => {
-      return <p id={''+title.id}>{title.id}</p>
-    })
-  }
 
-  return <section>
-    {users && renderTitles(users)}
-    {posts && renderTitles(posts)}
-  </section>
+  const handleChange = (event: SelectChangeEvent<string>) => {
+    setselectedUser(event.target.value as string);
+  };
+
+  if(users){
+    return (
+      <Box sx={{ minWidth: 120 }}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">User</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={selectedUser}
+            label="User"
+            onChange={handleChange}
+          >
+            {
+              users.map((user) => {
+                return (
+                  <MenuItem value={user.id}>{user.name}</MenuItem>
+                )
+              }) 
+            }
+          </Select>
+        </FormControl>
+      </Box> 
+    )}
+
+  return null;
 };
 
 export default Container;
