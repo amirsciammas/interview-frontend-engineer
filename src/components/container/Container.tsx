8c63c5e3a7af4f776cd182089e1a67e97778a9da
Mark Rivera <mark.rivera@wizeline.com>
2022-03-11 20:24:13 -0600

    [dev] Display Posts by User id

--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -1,24 +1,39 @@
 import { FunctionComponent, useEffect, useState } from "react";
 import { SelectChangeEvent } from '@mui/material/Select';
-import { getUsers } from "../../utils/api";
-import { User } from "../../utils/api.types";
+import { getUsers, getPostsByUserId } from "../../utils/api";
+import { User, Post } from "../../utils/api.types";
 import Users from "../users";
 
 const Container: FunctionComponent = () => {
-  const [users, setusers] = useState([] as User[])
-  const [selectedUser, setselectedUser] = useState('')
+  const [users, setusers] = useState([] as User[]);
+  const [selectedUser, setselectedUser] = useState('');
+  const [userPosts, setUserPosts] = useState([] as Post[])
 
   useEffect(() => {
-    getUsers().then(users => setusers(users as User[]));
+    getUsers().then(users => setusers(users as User[])).catch(() => setusers([] as User[]));
   }, []);
+
+  useEffect(() => {
+    getPostsByUserId(selectedUser).then(posts => setUserPosts(posts as Post[]))
+  }, [selectedUser])
 
   const onSelectUser = (event: SelectChangeEvent<string>) => {
     setselectedUser(event.target.value as string);
   };
 
-  if(users){
-    return <Users users={users} selectedUserId={selectedUser} onSelectUser={onSelectUser}/>}
-  return null;
+  const userPostsItems = userPosts.length ? userPosts.map((post => {
+      return <p key={post.id}> {post.body} </p>
+    })) : <p>No Posts</p>
+  
+
+  if(users.length){
+    return (
+      <>
+        <Users users={users} selectedUserId={selectedUser} onSelectUser={onSelectUser}/>
+        { userPostsItems}
+      </>
+    )}
+  return <p>No users</p>;
 };
 
 export default Container;
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
 
-const BASE_URL = 'https://jsonplaceholder.typicode.com'
+export const BASE_URL = 'https://jsonplaceholder.typicode.com'
 export const UserPostsEndpoints = {
   USERS: '/users',
   POSTS: '/posts'
@@ -11,7 +11,6 @@ function getResponseData<T extends object>(response: AxiosResponse<T>): T {
 }
 
 export const getSimpleEndpoint = <T extends object>(endpoint: string) : Promise<T> => {
-  console.log(endpoint)
   return axios
   .get(BASE_URL + endpoint)
   .then((response) => getResponseData(response))
@@ -25,13 +24,20 @@ export const getUsers = <T extends object>() : Promise<T> => {
   return getSimpleEndpoint(UserPostsEndpoints.USERS)
 }
 
-export const getPosts = <T extends object>() : Promise<T> => {
-  return getSimpleEndpoint(UserPostsEndpoints.POSTS)
-}
 
 export const getUser = <T extends object>(userId: string) : Promise<T> => {
   return axios
   .get(`${BASE_URL + UserPostsEndpoints.USERS}/${userId}`)
+  .then((response) => getResponseData(response))
+  .catch((error) => {
+    console.log(error.response);
+    throw error;
+  })
+}
+
+export const getPostsByUserId = <T extends object>(userId: string) : Promise<T> => {
+  return axios
+  .get(`${BASE_URL + UserPostsEndpoints.POSTS}/?userId=${userId}`)
   .then((response) => getResponseData(response))
   .catch((error) => {
     console.log(error.response);
