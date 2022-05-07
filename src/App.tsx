f186d68a956f8ddf9eaec88837a4b94cb23cb975
Indumathi Velan <indumathivelan@Indumathis-MacBook-Pro.local>
2022-05-07 16:33:01 +0200

    chore: Created Intial React project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = (): React.ReactElement => {
+  return (
+    <div>
+     Adidas FrontEnd Engineer Assignment
+    </div>
+  );
+}
+
+export default App;
