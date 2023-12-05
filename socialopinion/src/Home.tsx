07f727f1e99d13fd79aa33dcd2d5ce83ab215b2e
SimonGarbet <simon.garbet@gmail.com>
2023-12-05 16:39:44 +0100

    Switch User/Posts

--- socialopinion/src/Home.tsx
+++ socialopinion/src/Home.tsx
@@ -4,11 +4,24 @@ import Post from './components/Post';
 
 import styles from './home.module.css';
 
+type postObject =
+  {
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+  } []
+
 function App() {
 
   const [isDataLoading, setDataLoading] = useState(true);
-  const [postsList, setPostsList] = useState([{userId:0, id:0, title:'', body:''}]);
+  const [postsList, setPostsList] = useState<postObject>([{userId:0, id:0, title:'', body:''}]);
   const [error, setError] = useState(false);
+
+  const [postsVisibility, setPostsVisibility] = useState(true);
+  const [userVisibility, setUserVisibility] = useState(false);
+  const [userScope, setUserScope] = useState(1);
+  const [userPosts, setUserPosts] = useState<postObject>([{userId:0, id:0, title:'', body:''}]);
 
   useEffect(() => {
     async function fetchPosts() {
@@ -32,21 +45,60 @@ function App() {
     return <p>Error</p>
   } 
 
+  function switchPostsUser (param: string, newUserScope: number, postsList: postObject) {
+    if (param === 'userON'){
+      setPostsVisibility(false)
+      setUserVisibility(true)
+      setUserScope(newUserScope)
+      const userPosts = postsList.filter((post) => post.userId === newUserScope)
+      setUserPosts(userPosts)
+    }
+
+    if (param === 'postsON'){
+      setPostsVisibility(true)
+      setUserVisibility(false)
+    }
+  }
+
 
   return (
     <div className={styles.global}>
-      <h1>Social Opinion</h1>
+      <h1>Social<br/>Opinion</h1>
       { isDataLoading ? (
       <section> Chargement </section>
       ) : (
-        <section>
-          <h2>Posts</h2>
-          <div className='postContainer'>
-          {postsList?.map((post) => (
-            <Post key={post.id} title={post.title} author={post.userId} body={post.body}/>
-          ))}
-          </div>
-        </section>
+        <div>
+
+          <section style={{display : postsVisibility ? 'block' : 'none'}}>
+            <h2>Tous les Posts</h2>
+            <div>
+            {postsList?.map((post) => (
+              <Post onClick={() => switchPostsUser('userON', post.userId, postsList)}
+              key={post.id} 
+              title={post.title} 
+              author={post.userId} 
+              body={post.body}/>
+            ))}
+            </div>
+          </section>
+
+
+          <section style={{display : userVisibility ? 'block' : 'none'}}>
+            <button className={styles.switchButton} onClick={() => switchPostsUser('postsON', 1, postsList)}>Revenir sur les posts</button>
+            <h2>Profil de l'utilisateur {userScope}</h2>
+            <div>
+            {userPosts?.map((post) => (
+              <Post
+              key={post.id} 
+              title={post.title} 
+              author={post.userId} 
+              body={post.body}/>
+            ))}
+            </div>
+          </section>
+
+
+        </div>
       )}
         
     </div>
--- socialopinion/src/components/Post/index.tsx
+++ socialopinion/src/components/Post/index.tsx
@@ -4,13 +4,14 @@ type postProps = {
     title: string;
     author: number;
     body: string;
+    onClick?: () => void;
 }
 
-function Post({title, author, body}: postProps) {
+function Post({title, author, body, onClick}: postProps) {
 
   return (
     <div className={styles.post}>
-        <div>
+        <div onClick={onClick}>
         <h3>{title}</h3>
         <p>{body}</p>
         </div>
